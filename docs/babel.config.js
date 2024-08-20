6ad2c5a9e5e2a351ecb0a1deb75a96fb8252b8ee
tetarchus <8436118+tetarchus@users.noreply.github.com>
2024-08-20 02:15:33 +0100

    fix: resolve incorrect tsconfig path

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
-  plugins: [['babel-plugin-tsconfig-paths', { tsconfig: 'docs/tsconfig.json' }]],
+  plugins: [['babel-plugin-tsconfig-paths', { tsconfig: 'tsconfig.json' }]],
 };
