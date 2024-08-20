34de4e3a9a02d4b81156aab6613bb9eee2be7649
tetarchus <8436118+tetarchus@users.noreply.github.com>
2024-08-20 02:11:30 +0100

    fix: resolve typechecking errors

--- src/manager.tsx
+++ src/manager.tsx
@@ -1,16 +1,16 @@
 import { addons, types } from 'storybook/internal/manager-api';
 
 import { Tool } from '@/components';
-import { ADDON_ID, ADDON_TITLE, BADGES_KEY, TOOL_ID } from '@/constants';
+import { ADDON_ID, ADDON_TITLE, PARAM_BADGES_KEY, TOOL_ID } from '@/constants';
 
 // Register the addon
-addons.register(ADDON_ID, api => {
+addons.register(ADDON_ID, _api => {
   // Register a tool
   addons.add(TOOL_ID, {
     title: ADDON_TITLE,
     type: types.TOOL,
-    paramKey: BADGES_KEY,
+    paramKey: PARAM_BADGES_KEY,
     match: () => true,
-    render: () => <Tool api={api} />,
+    render: () => <Tool />,
   });
 });
