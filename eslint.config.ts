29f909c958d638e6c694a68b0c941932b47d01d8
tetarchus <8436118+tetarchus@users.noreply.github.com>
2024-10-11 00:40:30 +0100

    chore: update project configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,6 +6,18 @@ import pluginReactHooks from 'eslint-plugin-react-hooks';
 
 const eslintConfig = [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    ignores: [
+      '.storybook/local-preset.js',
+      '.vscode/**/*',
+      '**/.docusaurus/**/*',
+      '**/*.config.cjs',
+      '**/build/**/*',
+      'dist/**/*',
+      'preset.js',
+      'scripts/**/*',
+    ],
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
